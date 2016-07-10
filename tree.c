a2eb852ab97dafc682f20facf6ac35e323ba77e6
boviya <boviya09526@gmail.com>
2016-07-10 11:54:13 +0530

    finish

---
+++ tree.c
@@ -0,0 +1,30 @@
+# oviya1# oviya
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int length,thickness,a;
+printf("enter the length of the branch");
+scanf("%d",&length);
+printf("enter the length of the thickness");
+scanf("%d",&thickness);
+if(length<<5)
+{
+printf("The branch is very small");
+printf("color of the branch is green");
+}
+else
+{
+printf("the color of the branch is not green");
+}
+if(length<<5)
+{
+a=thickness-2
+printf("thickness of the branch decreased by %d",thickness);
+}
+else
+{
+printf("thickness of the branch remains same");
+}
+getch();
+}
